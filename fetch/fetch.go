b07fe71804de9b8b04b6c8779f04d617729660f5
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2024-11-15 20:25:19 +0300

    метод fetch доработан

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -5,21 +5,28 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func Get() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+			fmt.Println("Добавлен префикс, url = " + url)
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
+
+		fmt.Printf("Http request responce status code: %v\n", resp.Status)
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
 			fmt.Fprintf(os.Stderr, "fetch чтение %s: %v", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		resp.Body.Close()
 	}
 }
